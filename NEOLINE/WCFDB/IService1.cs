2dcf91a0f09d6563ad7e41a339911e52c56cf865
Xam <Xam@A24COMP6>
2017-12-20 17:25:16 +0200

    Create DB

---
+++ NEOLINE/WCFDB/IService1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFForOnlineShopCenter
+{
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        void RegistrationUser(string login, string pass, string nick, string email);
+
+        [OperationContract]
+        void AddProduct(string productName, string Descr, string Photo);
+
+        [OperationContract]
+        void AddAdmin(string login, string pass, string email);
+    }
+}
---
+++ NEOLINE/WCFDB/Service1.svc.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WCFForOnlineShopCenter
+{
+    public class Service1 : IService1
+    {
+        public void RegistrationUser(string login, string pass, string nick, string email)
+        {
+            using (OnlineShop user = new OnlineShop())
+            {
+                user.Users.Add(new User()
+                {
+                    Login = login,
+                    Password = pass,
+                    Nickname = nick,
+                    Email = email
+                });
+
+                user.SaveChanges();
+            }
+        }
+
+        public void AddProduct(string productName, string descr, string photo)
+        {
+            using (OnlineShop user = new OnlineShop())
+            {
+                user.Products.Add(new Product()
+                {
+                    ProductName = productName,
+                    Description = descr,
+                    Photo = photo
+                });
+
+                user.SaveChanges();
+            }
+        }
+
+        public void AddAdmin(string login, string pass, string email)
+        {
+            using (OnlineShop user = new OnlineShop())
+            {
+                user.Admins.Add(new Admin()
+                {
+                    Login = login,
+                    Password = pass,
+                    Email = email
+                });
+
+                user.SaveChanges();
+            }
+        }
+    }
+}
